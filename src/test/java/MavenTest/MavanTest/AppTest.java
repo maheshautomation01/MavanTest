5bd14aa0011eefef95db406328989f17c12c7a80
Mahesh <maheshwaran.msm@gmail.com>
2018-04-16 21:16:27 -0400

    new project

---
+++ src/test/java/MavenTest/MavanTest/AppTest.java
@@ -0,0 +1,26 @@
+package MavenTest.MavanTest;
+
+import static org.junit.Assert.assertTrue;
+
+import org.testng.annotations.Test;
+
+
+
+
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+        
+        System.out.println("Mahedsh Test is Pass");
+    }
+}
